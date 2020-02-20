68294a78c91d4da19b7c3ac41e984306822843d2
arcanar7 <arcanar7@gmail.com>
2020-02-20 10:36:20 +0400

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-module.exports = function toReadable (number) {
-  
+const ones = [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+];
+const tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+];
+const teens = [
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+];
+
+module.exports = function toReadable(number) {
+    if (number == 0) return "zero";
+
+    if (number > 99) return `${ones[Math.floor(number / 100)]} hundred ${convert_tens(number % 100)}`.trimRight();
+    else return convert_tens(number);
+};
+
+function convert_tens(number) {
+    if (number < 10) return ones[number];
+    else if (number >= 10 && number < 20) return teens[number - 10];
+    else return `${tens[Math.floor(number / 10)]} ${ones[number % 10]}`.trimRight();
 }
