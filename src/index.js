f3c081015450684303b71006578c1e6f450f0ab1
arcanar7 <arcanar7@gmail.com>
2020-02-20 10:38:16 +0400

    refactor: change function name

--- src/index.js
+++ src/index.js
@@ -38,11 +38,11 @@ const teens = [
 module.exports = function toReadable(number) {
     if (number == 0) return "zero";
 
-    if (number > 99) return `${ones[Math.floor(number / 100)]} hundred ${convert_tens(number % 100)}`.trimRight();
-    else return convert_tens(number);
+    if (number > 99) return `${ones[Math.floor(number / 100)]} hundred ${convertTens(number % 100)}`.trimRight();
+    else return convertTens(number);
 };
 
-function convert_tens(number) {
+function convertTens(number) {
     if (number < 10) return ones[number];
     else if (number >= 10 && number < 20) return teens[number - 10];
     else return `${tens[Math.floor(number / 10)]} ${ones[number % 10]}`.trimRight();
